62bd16d42e96424cb2ab4cf1684ae526b36a1598
Maxim <maxofon@tut.by>
2020-12-11 09:22:16 +0300

    Added MainModuleTests.cs

---
+++ NancyWebApiCore.Tests/MainModuleTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Nancy;
+using Nancy.Testing;
+using NancyWebApiCore.Entities;
+using NancyWebApiCore.Helpers;
+using NancyWebApiCore.Interfaces;
+using NancyWebApiCore.Services;
+using NancyWebApiCore.Views;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace NancyWebApiCore.Tests
+{
+    [TestFixture]
+    public class MainModuleTests
+    {
+        private Browser _browser;
+        private Mock<IAppConfiguration> _config;
+
+        [SetUp]
+        public void Setup()
+        {
+            _config = new Mock<IAppConfiguration>();
+            
+                
+            var bootsrapper = new Bootstrapper(_config.Object);
+            _browser = new Browser(bootsrapper);
+        }
+
+        [Test]
+        public async Task GetHome_ApiKeyIsValid_ReturnServiceIsWorking()
+        {
+            //Arrange
+            _config.Setup(x => x.NytSettings)
+                .Returns(new NytSettings { ApiKey = "k0XA0k0jJGAVuv8Jr5wAIcKDGPuznmRJ", BaseUrl = "https://api.nytimes.com/svc/topstories/v2/{0}.json?api-key={1}", DefaultSection = "home" });
+
+            //Act
+            var response = await _browser.Get("/", (with) => {
+                with.HttpRequest();
+            });
+
+            // Assert
+            Assert.That(HttpStatusCode.OK, Is.EqualTo(response.StatusCode));
+            Assert.That(response.Body.AsString().Contains("is working"));
+        }
+
+        [Test]
+        public async Task GetHome_ApiKeyIsNotValid_ReturnServiceIsNotWorking()
+        {
+            //Arrange
+            _config.Setup(x => x.NytSettings)
+                .Returns(new NytSettings { ApiKey = "not_valid_key", BaseUrl = "https://api.nytimes.com/svc/topstories/v2/{0}.json?api-key={1}", DefaultSection = "home" });
+
+            //Act
+            var response = await _browser.Get("/", (with) => {
+                with.HttpRequest();
+            });
+
+            // Assert
+            Assert.That(HttpStatusCode.OK, Is.EqualTo(response.StatusCode));
+            Assert.That(response.Body.AsString().Contains("is not working"));
+        }
+
+        [Test]
+        public async Task GetListSection_SectionPassed_ReturnJson()
+        {
+            //Arrange
+            _config.Setup(x => x.NytSettings)
+                .Returns(new NytSettings { ApiKey = "k0XA0k0jJGAVuv8Jr5wAIcKDGPuznmRJ", BaseUrl = "https://api.nytimes.com/svc/topstories/v2/{0}.json?api-key={1}", DefaultSection = "home" });
+            var section = "health";
+
+            //Act
+            var response = await _browser.Get($"/list/{section}", (with) => {
+                with.HttpRequest();
+            });
+
+            var resultString = response.Body.AsString();
+            var items = JsonConvert.DeserializeObject<ArticleView[]>(resultString);
+
+            // Assert
+            Assert.That(HttpStatusCode.OK, Is.EqualTo(response.StatusCode));
+            Assert.That(items.Length > 0, Is.True);
+        }
+
+        [Test]
+        public async Task GetListSectionFirst_SectionPassed_ReturnJson()
+        {
+            //Arrange
+            _config.Setup(x => x.NytSettings)
+                .Returns(new NytSettings { ApiKey = "k0XA0k0jJGAVuv8Jr5wAIcKDGPuznmRJ", 
+                    BaseUrl = "https://api.nytimes.com/svc/topstories/v2/{0}.json?api-key={1}", DefaultSection = "home" });
+            var section = "health";
+
+            //Act
+            var response = await _browser.Get($"/list/{section}/first", (with) => {
+                with.HttpRequest();
+            });
+
+            var resultString = response.Body.AsString();
+            var items = JsonConvert.DeserializeObject<ArticleView>(resultString);
+
+            // Assert
+            Assert.That(HttpStatusCode.OK, Is.EqualTo(response.StatusCode));
+        }
+
+    }
+}
